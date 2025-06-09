89df95ffe9aca61c36a23b96ef88d81258400253
deepmali890 <deepmali590@gmail.com>
2025-06-09 23:20:21 +0530

    cookie fix

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -132,9 +132,9 @@ exports.login = async (req, res) => {
         // Send token as HTTP-only cookie
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', // only over HTTPS in prod
-            sameSite: 'lax',
-            maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+            secure: true,
+            sameSite: 'none',
+            maxAge: 24 * 60 * 60 * 1000,
         });
         res.status(200).json({ message: 'Login successful!', token, userId: user._id, role: user.role, success: true })
 
